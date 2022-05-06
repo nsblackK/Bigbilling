6b8b67bedb441651908d0cf53eeaa6365ac06c7a
nsblackK <nsblack.2001@gmail.com>
2022-05-06 14:17:01 +0530

    this is the 2nd test commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                        requires org.kordamp.bootstrapfx.core;
-            
+
+    requires org.kordamp.bootstrapfx.core;
+
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
 }
