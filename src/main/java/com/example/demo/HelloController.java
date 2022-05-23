46e4863e82235e4165a35565ba0aafa2bc40b1a5
nsblackK <nsblack.2001@gmail.com>
2022-05-23 22:35:10 +0530

    almost done

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,13 +2,58 @@ package com.example.demo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
+    int TotalCost = 0;
+    String TC = "";
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void Lays() {
+        TotalCost = TotalCost + 20;
+        System.out.println("bought 1 lays"+" total cost: "+ TotalCost);
+        TC = new String(String.valueOf(TotalCost));
     }
+    @FXML
+    protected void Coffee() {
+        TotalCost = TotalCost + 40;
+        System.out.println("bought 1 coffee"+" total cost: "+ TotalCost);
+        TC = new String(String.valueOf(TotalCost));
+    }
+
+    @FXML
+    protected void wafers() {
+        TotalCost = TotalCost + 50;
+        System.out.println("bought 1 wefer"+" total cost: "+ TotalCost);
+        TC = new String(String.valueOf(TotalCost));
+    }
+
+
+    @FXML
+    protected void pay() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("PAGE 3.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Stage stage = new Stage();
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();}
+
+    @FXML
+    protected void HUNGRY() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Page 2.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Stage stage = new Stage();
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
 }
